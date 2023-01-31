5e4b9233f5752645aefcf27183c482f71b232879
Amitaifink <amitai38@hotmail.com>
2023-01-31 12:14:08 +0200

    ש

--- Client/social-keeper-client/App.js
+++ Client/social-keeper-client/App.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import * as Contacts from 'expo-contacts';
 
-
 import MainDashBoard from './Component/MainDashBoard';
 
 export default function App() {
@@ -13,22 +12,20 @@ export default function App() {
 
   //this section is for the contact list
   //there is more work to be done here to get the contact list to work
-  // useEffect(() => {
-  //   (async () => {
-  //     const { status } = await Contacts.requestPermissionsAsync();
-  //     if (status === 'granted') {
-  //       const { data } = await Contacts.getContactsAsync({
-  //         fields: [Contacts.Fields.PhoneNumbers],
-  //       });
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.PhoneNumbers],
+        });
 
-  //       if (data.length > 0) {
-  //         setAllContacts(data);
-          
-
-  //       }
-  //     }
-  //   })();
-  // }, [console.log(allContacts)]);
+        if (data.length > 0) {
+          setAllContacts(data);        
+        }
+      }
+    })();
+  }, [console.log(allContacts)]);
 
 
   return (
