82f1f44bc1fb8bc56f33c6d3633051419c75cbcc
dor933 <doratzabi1@gmail.com>
2023-03-07 19:57:08 +0200

    asdsd

--- Client/social-keeper-client/Component/SettingDashBoard.jsx
+++ Client/social-keeper-client/Component/SettingDashBoard.jsx
@@ -186,7 +186,7 @@ const Stack = createNativeStackNavigator();
 export default function SettingDashBoard(props) {
   return (
     <Text>
-      settnigs try to change2sasdasdasdss
+      settnigs try to change2sasdasdasdssss
     </Text>
     // <NavigationContainer independent={true}
     // >
