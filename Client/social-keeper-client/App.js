77a08a00f050be05db4197112e6e3f8dfc2db948
TalSofir <110484239+TalSofir@users.noreply.github.com>
2023-04-21 15:43:20 +0300

    commit with maps compnent

--- Client/social-keeper-client/App.js
+++ Client/social-keeper-client/App.js
@@ -1,7 +1,21 @@
 import { SafeAreaView, StyleSheet} from 'react-native';
 import CreateProfile from './Components/Screens/Profile/CreateProfile';
+<<<<<<< Updated upstream
+=======
+import PreferredMeetingTimes from './Components/Screens/Settings/PreferredMeetingTimes';
+import PrefferedHobbies from './/Components/Screens/Settings//PreferredHoobies';
+import MainDashBoard from './Components/Screens//MainApp/MainDashBoard';
+import FutureMeetingScreen from './Components/Screens//MainApp/FutureMeetingScreen';
+import PreviousMeetingsScreen from './Components/Screens//MainApp/PreviousMeetingsScreen';
+import MainAppcontext from './Components/Screens/MainApp/MainAppcontext';
+import { Text, View } from 'react-native';
+import MapLocationForHobbies from './Components/Screens/MainApp/MapLocationForHobbies';
+const Registrationstack = createStackNavigator();
+const Mainappstack = createStackNavigator();
+>>>>>>> Stashed changes
 
 
+<<<<<<< Updated upstream
 
 //Imports to use :
 // import SignInAPI from './Components/Screens/Login/SignInAPI';
@@ -12,17 +26,59 @@ import CreateProfile from './Components/Screens/Profile/CreateProfile';
 // import PreviousMeetingsScreen from './/Components//Screens//Meetings///PreviousMeetingsScreen';
 // import SuggestedMeetingsScreen from './/Components//Screens//Meetings///SuggestedMeetingsScreen';
 // import FavoriteContacts from './/Components//Screens///Settings/FavoriteContacts';
+=======
+    <Registrationstack.Navigator initialRouteName="SignIn">
+
+      <Registrationstack.Screen name="SignIn" component={SignInAPI} options={{ headerShown: false }} />
+      <Registrationstack.Screen name="CreateProfile" component={CreateProfile} options={{ headerShown: false }} />
+      <Registrationstack.Screen name="PreferredMeetingTimes" component={PreferredMeetingTimes} options={{ headerShown: false }} />
+      <Registrationstack.Screen name="FavoriteContacts" component={FavoriteContacts} options={{ headerShown: false }} />
+      <Registrationstack.Screen name="PreferredHoobies" component={PrefferedHobbies} options={{ headerShown: false }} />
+
+    </Registrationstack.Navigator>
+>>>>>>> Stashed changes
 
 
+<<<<<<< Updated upstream
+=======
+const MainAppStackScreen = () => {
+  return (
+    <Mainappstack.Navigator initialRouteName="MainDashBoard" >
+      <Mainappstack.Screen name="MainDashBoard" component={MainDashBoard} options={{ headerShown: false }} />
+      <Mainappstack.Screen name="FutureMeetingScreen" component={FutureMeetingScreen} options={{ headerShown: false }} />
+      <Mainappstack.Screen name="PreviousMeetingsScreen" component={PreviousMeetingsScreen} options={{ headerShown: false }} />
+
+    </Mainappstack.Navigator>
+  )
+}
+>>>>>>> Stashed changes
 
 
 export default function App() {
 
 
   return (
+<<<<<<< Updated upstream
     <SafeAreaView>
       <CreateProfile />
     </SafeAreaView>
+=======
+
+    // <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+    // <RegistrationContext>
+    //   <MainAppcontext>
+
+    //   <NavigationContainer>
+    //     {isAuthenticated ?  <MainAppStackScreen />  : ( <RegistrationStackScreen /> ) }
+    //   </NavigationContainer>
+    //   </MainAppcontext>
+
+    // </RegistrationContext>
+    // </AuthContext.Provider>
+    <MapLocationForHobbies></MapLocationForHobbies>
+
+
+>>>>>>> Stashed changes
   );
 }
 
--- Client/social-keeper-client/Components/Screens/Login/SignInAPI.jsx
+++ Client/social-keeper-client/Components/Screens/Login/SignInAPI.jsx
@@ -1,11 +1,143 @@
 import React from 'react'
 import { SafeAreaView, Image, Text, StyleSheet, View, TouchableOpacity } from 'react-native'
+<<<<<<< Updated upstream
+=======
+import * as Google from "expo-auth-session/providers/google";
+import * as AuthSession from 'expo-auth-session';
+import * as Facebook from 'expo-auth-session/providers/facebook';
+import * as WebBrowser from 'expo-web-browser';
+import { useEffect, useState, useContext } from "react";
+import axios from 'axios';
+import { RegistContext } from '..//../..//RegistContext.jsx';
+import { MainAppcontext } from '../MainApp/MainAppcontext.jsx';
+import AuthContext from '../../../Authcontext.jsx';
+
+
+function SignUpAPI({ navigation }) {
+
+  const { personaldetails, setPersonalDetails } = useContext(RegistContext);
+  const { setUser } = useContext(MainAppcontext);
+  const { setIsAuthenticated } = React.useContext(AuthContext);
+
+  WebBrowser.maybeCompleteAuthSession();
+
+  const [token, setToken] = useState("");
+  const [userInfo, setUserInfo] = useState(null);
+  const RedirectUrl = AuthSession.makeRedirectUri({
+    useProxy: true,
+
+  });
+  console.log(RedirectUrl);
+
+
+
+  const [request2, response2, promptAsync2] = Facebook.useAuthRequest({
+    clientId: '177882227985650',
+    useProxy: true,
+    redirectUri: RedirectUrl,
+    scopes: ['public_profile', 'email'],
+  });
+
+
+  // Use expo's web browser
+
+
+
+
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '923332378077-0gf55cn5cq0dvpahm5bk6vetdaigl7cr.apps.googleusercontent.com',
+
+    // Use expo's web browser
+
+    useProxy: true,
+    redirectUri: RedirectUrl,
+  });
+
+  useEffect(() => {
+    console.log('into edffect');
+    if (response?.type === "success") {
+      console.log('got here');
+      console.log(response);
+      setToken(response.authentication.accessToken);
+      getUserInfo();
+    }
+  }, [response, token]);
+
+  useEffect(() => {
+    console.log('into edffect');
+    if (response2?.type === "success") {
+      console.log('got here');
+      console.log(response2);
+      setToken(response2.authentication.accessToken);
+      getuserinfofromfacebook();
+    }
+  }, [response2, token]);
+
+  const getUserInfo = async () => {
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      const user = await response.json();
+      setUserInfo(user);
+      console.log(user);
+      const ifuser = await axios.post('http://cgroup92@194.90.158.74/cgroup92/prod/api/Default/Signin', { email: user.email });
+      if (ifuser.data == 'no user was found') {
+
+        if (user.email != undefined) {
+
+          personaldetails.email = user.email;
+          navigation.navigate('CreateProfile');
+        }
+      }
+      else if (typeof ifuser.data.imageUri == 'string') {
+        setUser(ifuser.data);
+        setIsAuthenticated(true);
+      }
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
+
+  const getuserinfofromfacebook = async () => {
+    try {
+
+      console.log(token);
+      console.log('got here');
+      const response = await axios.get(`https://graph.facebook.com/me?fields=email,id,name&access_token=${token}`)
+      const user = response.data;
+      setUserInfo(user);
+      console.log(user);
+      const ifuser = await axios.post('http://cgroup92@194.90.158.74/cgroup92/prod/api/Default/Signin', { email: user.email });
+      if (ifuser.data == 'no user was found') {
+
+        personaldetails.email = user.email;
+        navigation.navigate('CreateProfile');
+      }
+      else if (typeof ifuser.data.imageUri == 'string') {
+        setUser(ifuser.data);
+        console.log(ifuser.data);
+        setIsAuthenticated(true);
+      }
+
+    } catch (error) {
+      // Add your own error handler here
+    }
+  }
+
+
+>>>>>>> Stashed changes
 
 function SignInAPI() {
 
 
   return (
 
+<<<<<<< Updated upstream
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.elipseTop}></View>
 
@@ -30,6 +162,34 @@ function SignInAPI() {
       </TouchableOpacity>
 
       <View style={styles.elipseButtom}></View>
+=======
+    <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.elipseTop}></View>
+        {/* Connect text */}
+        <Text style={styles.text}>Sign In</Text>
+        <Image style={styles.logo} source={require('../../../assets/Images/RandomImages/social-keeper-low-resolution-logo-color-on-transparent-background.png')} />
+
+        {/* Login with google button */}
+        <TouchableOpacity onPress={async () => promptAsync()}>
+          <View style={styles.container}>
+            <Image style={styles.googleLogo} source={require('../../../assets/Images/Logos/GoogleLogo.png')} ></Image>
+            <Text style={styles.textForButton}>Sign in with Google</Text>
+          </View>
+        </TouchableOpacity>
+
+        {/* Login with outlook button */}
+        <TouchableOpacity onPress={async () => promptAsync2()}>
+          <View style={styles.container2}>
+            <Image style={styles.outLookLogo} source={require('../../../assets/Images/Logos/facebook.png')} ></Image>
+            <Text style={styles.textForButton}>Login with Facebook</Text>
+          </View>
+        </TouchableOpacity>
+
+
+        <View style={styles.elipseButtom}></View>
+      </SafeAreaView>
+>>>>>>> Stashed changes
     </SafeAreaView>
   )
 }
@@ -101,6 +261,10 @@ const styles = StyleSheet.create({
     height: 932,
     backgroundColor: '#FFF1F1',
     borderRadius: 50,
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
   },
 
 
@@ -144,7 +308,11 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: 505,
     height: 364,
+<<<<<<< Updated upstream
     left: 100,
+=======
+    left: 212,
+>>>>>>> Stashed changes
     top: 650,
     borderRadius: 200,
     backgroundColor: '#FFAEAE',
